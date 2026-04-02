580756a5caa9482ffd42417747c121b6f4423c7b
spitefully-positive <121156572+spitefully-positive@users.noreply.github.com>
2026-04-02 23:01:34 +0200

    Extended Astro config to enable deployments with node

---
+++ astro.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+
+// https://astro.build/config
+export default defineConfig({
+  // #Todo add your domain name here, when you want to deploy
+  // site: 'https://my-resume.rubens-super-cool-domain.com',
+  adapter: node({
+    mode: 'standalone',
+  }),
+});
