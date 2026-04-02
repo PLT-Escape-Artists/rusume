8ec909c71dc3ef84fc63bfea257453cc209696d0
spitefully-positive <121156572+spitefully-positive@users.noreply.github.com>
2026-04-02 23:05:10 +0200

    Added a useless file to make build warnig go away

---
+++ src/content.config.ts
@@ -0,0 +1,5 @@
+// You could create content collections here. Awsome feature! I don't need it for my resume!
+// This file just exists, so the build warning disappears...
+// Read more about content collections here: https://docs.astro.build/guides/content-collections/
+
+export const collections = {};
