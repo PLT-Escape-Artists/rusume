8d59d362c292723bded4fe52405181fce4cc18ab
spitefully-positive <121156572+spitefully-positive@users.noreply.github.com>
2026-04-02 23:03:34 +0200

    Added formatting and linting to your codebase with prettier and astro check

---
+++ prettier.config.ts
@@ -0,0 +1,19 @@
+import { type Config } from 'prettier';
+
+const config: Config = {
+  plugins: ['prettier-plugin-astro'],
+  overrides: [{ files: '*.astro', options: { parser: 'astro' } }],
+
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 4,
+
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: true,
+  trailingComma: 'es5',
+  bracketSameLine: true,
+  endOfLine: 'lf',
+};
+
+export default config;
